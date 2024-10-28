4627f474390ac95977273f8effee9feaf62d270f
Rodrix0 <rodrigomagallanes0@gmail.com>
2024-10-28 20:31:05 -0300

    El grpc se pudo realizar falta realizar el inicio de server

--- src/main/java/com/example/gestordecorreos/GrpsService.java
+++ src/main/java/com/example/gestordecorreos/GrpsService.java
@@ -1,12 +1,14 @@
 package com.example.gestordecorreos;
 
+import com.example.grpc.generated.GrpsServiceGrpc.GrpsServiceImplBase;
 import com.google.rpc.context.AttributeContext.Request;
 import com.google.rpc.context.AttributeContext.Response;
 
 
 import io.grpc.stub.StreamObserver;
 
-public class GrpsService extends GrpsService.GrpsServiceImplBase {
+public class GrpsService extends GrpsServiceImplBase.GrpsServiceImplBase {
+
     @Override
     public void someMethod(Request request, StreamObserver<Response> responseObserver) {
         // Lógica del servicio
@@ -14,4 +16,5 @@ public class GrpsService extends GrpsService.GrpsServiceImplBase {
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+    
 }
